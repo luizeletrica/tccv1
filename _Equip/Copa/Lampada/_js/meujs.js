e20309a86266c61e66d7f58c74d204385388b103
luizeletrica <46749636+luizeletrica@users.noreply.github.com>
2019-09-05 14:12:32 -0300

    Update meujs.js

--- _Equip/Copa/Lampada/_js/meujs.js
+++ _Equip/Copa/Lampada/_js/meujs.js
@@ -3,10 +3,10 @@ function AltRele_COPA1(){
     if(estado === "0"){
         document.getElementById("estado").innerHTML="1";
         document.getElementById("rele").innerHTML="<div class='rele1_ligado'></div>";
-		document.getElementById("botao").innerHTML="<a href='/?desligar' class='botao'>Desliga</a>";
+		document.getElementById("botao").innerHTML="<a href='/monitor/?desligar' class='botao'>Desliga</a>";
     } else {
         document.getElementById("estado").innerHTML="0";
         document.getElementById("rele").innerHTML="<div class='rele1_desligado'></div>";
-        document.getElementById("botao").innerHTML="<a href='/?ligar' class='botao'>Liga</a>";
+        document.getElementById("botao").innerHTML="<a href='/monitor/?ligar' class='botao'>Liga</a>";
     }
 }
